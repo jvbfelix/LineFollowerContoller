4b4066a30498af1b514825a8612e9969609027c1
João Felix <jvbf@cin.ufpe.br>
2020-12-21 14:11:14 -0300

    interface base

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Olá, está é a pagina secreta do Cininho, talvez tenhamos algo por aqui em breve!</h1>
+    <img alt="RoboCin logo" class="RCLogo2" src="../assets/logo.png">
   </div>
 </template>
+
+<style lang="scss">
+.RCLogo2 {
+  max-width: 80vw;
+  max-height: 80vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,116 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+    <img alt="RoboCin logo" class="RCLogo" src="../assets/logo.png">
+    <div class="controle">
+      <the-mask type="text" name="pwma" v-model="pwma" placeholder="0.00" mask="#.##"></the-mask>
+      <button>Atualizar PWMA</button>
+      <the-mask type="text" name="pwmb" v-model="pwmb" placeholder="0.00" mask="#.##"></the-mask>
+      <button>Atualizar PWMB</button>
+      <the-mask type="text" name="pwmca" v-model="pwmca" placeholder="0.00" mask="#.##"></the-mask>
+      <button>Atualizar PWMA Curva</button>
+      <the-mask type="text" name="pwmcb" v-model="pwmcb" placeholder="0.00" mask="#.##"></the-mask>
+      <button>Atualizar PWMB Curva</button>
+    </div>
+    <div class="status">
+      <p>Motor A:</p>
+      <p>Motor B:</p>
+      <p>Motor A Curva:</p>
+      <p>Motor B Curva:</p>
+      <p>Bateria:</p>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import {TheMask} from 'vue-the-mask'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TheMask
+    //HelloWorld
   }
 }
 </script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+.container{
+  background-color: #000000AA;
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+  &::-webkit-scrollbar {
+    width: 10px;
+    border-radius: 15px;
+  }
+  &::-webkit-scrollbar-track {
+    background: #CACACA;
+    border-radius: 20px;
+    border: 2px #FFFFFF solid;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #174C74;
+    border-radius: 20px;
+  }
+  
+  &::-webkit-scrollbar-thumb:hover {
+    background: #174C74CC;
+  }  
+}
+.home {
+  background-image: url('../assets/back.jpg');
+  background-size: cover;
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+}
+
+.RCLogo {
+  width: 200px;
+  max-width: 80%;
+}
+.controle {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  input {
+    width: 500px;
+    max-width: 80%;
+    margin: 10px;
+    padding: 5px 15px;
+    border-radius: 15px;
+    border: 1px solid #000000;
+  }
+  button {
+    width: fit-content;
+    margin-bottom: 15px;
+    background-color: #C4C4C4;
+    border-radius: 15px;
+    border: 1px solid #000000;
+    padding: 5px 15px;
+    cursor: pointer;
+  }
+  input:focus{
+    outline: 0;
+  }
+  button:focus{
+    outline: 0;
+  }
+}
+
+.status {
+  text-align: left;
+  color: #FFFFFF;
+  padding: 25px;
+}
+</style>
